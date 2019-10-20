34b0ecf0464e6ec26c2a6d152669364364c020b9
Claudio Costa <claudio@imaginaction.com.br>
2019-10-20 19:34:20 -0300

    feet: 02 - 03 - Criando o primeiro teste

---
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.leilao.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeilaoTest {
+
+    @Test
+    public void getDescricao() {
+        //Criar cenário de teste
+        Leilao console = new Leilao("Console");
+        //Executar ação esperada
+        String descricaoDevolvida = console.getDescricao();
+        //Testar resultado esperado
+        assertEquals("Console", descricaoDevolvida);
+    }
+}
